0447729def56af3d699aaedafa9dbcae7a320d0e
Stefanie Duwe <fafidw@gmail.com>
2022-07-19 13:43:30 -0300

    exercícios realizados

---
+++ exercicio1/script.js
@@ -0,0 +1,13 @@
+// Apresenta erro de sintaxe, pois não foi definido um valor para a variável.
+
+const nome = prompt("Qual é seu nome?");
+let idade = prompt("Qual a sua idade?");
+
+console.log(nome, idade);
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+//Ele considera as letras e números como string
+
+console.log("Olá!",nome,", você tem",idade,"anos.")
---
+++ exercicio2/script.js
@@ -0,0 +1,16 @@
+let cidade = prompt("Você mora em Jaraguá do Sul?");
+let casado = prompt("Você é casado(a)?");
+let programacao = prompt("Você é estudante de programção?")
+
+let rcidade = sim;
+let rcasado = não;
+let rprogramacao = sim;
+
+console.log(rcidade, rcasado, rprogramacao)
+
+/*let rcidade = prompt("Você mora em Jaraguá do Sul?");
+
+let cidade = "Você mora em Jaraguá do sul?";
+let rcidade = prompt(cidade);*/
+
+console.log(cidade, rcidade);
